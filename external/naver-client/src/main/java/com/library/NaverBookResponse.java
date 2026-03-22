25997f25a599dc62dcf7a8d3fa53baef953fdf7d
gyucheol <gyucheol@talktomeinkorean.com>
2026-03-22 10:59:47 +0900

    [#2, LS-4] 네이버 클라이언트 연동 - 네이버 응답 형식 적용

---
+++ external/naver-client/src/main/java/com/library/NaverBookResponse.java
@@ -0,0 +1,17 @@
+package com.library;
+
+import lombok.Getter;
+import lombok.ToString;
+
+import java.util.List;
+
+@Getter
+@ToString
+public class NaverBookResponse {
+    private String lastBuildDate;
+    private int total;
+    private int start;
+    private int display;
+    private List<Item> items;
+
+}
---
+++ external/naver-client/src/test/groovy/com/library/feign/NaverClientIntegrationTest.groovy
@@ -0,0 +1,34 @@
+package com.library.feign
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.cloud.openfeign.EnableFeignClients
+import org.springframework.test.context.ActiveProfiles
+import spock.lang.Ignore
+import spock.lang.Specification
+
+// 이런 외부 테스트는 결과가 그때그때 달라서 테스트가 불안정할 수 있기 때문에, 일단은 @Ignore 처리해놓고, 필요할 때마다 켜서 테스트하자.
+@Ignore
+@SpringBootTest(classes = NaverClientIntegrationTest.TestConfig.class)
+@ActiveProfiles("test")
+class NaverClientIntegrationTest extends Specification {
+
+    @EnableAutoConfiguration
+    @EnableFeignClients(clients = NaverClient.class)
+    static class TestConfig{}
+    
+    @Autowired
+    NaverClient naverClient
+    
+    def "네이버 호출"() {
+        given:
+
+        when:
+        def response = naverClient.search("HTTP", 1, 10)
+
+        then:
+        print response
+    }
+
+}
