08a2bbfb8182ff070e0c49dcb689587c8c6040c4
gyucheol <gyucheol@talktomeinkorean.com>
2026-03-23 22:45:33 +0900

    [#2, LS-4] 네이버 클라이언트 연동 - 공통모듈 예외 정의

--- external/naver-client/src/test/groovy/com/library/feign/NaverErrorDecoderTest.groovy
+++ external/naver-client/src/test/groovy/com/library/feign/NaverErrorDecoderTest.groovy
@@ -1,16 +1,19 @@
 package com.library.feign
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.library.ApiException
+import com.library.ErrorType
 import com.library.NaverErrorResponse
 import feign.Request
 import feign.Response
+import org.springframework.http.HttpStatus
 import spock.lang.Specification
 
 class NaverErrorDecoderTest extends Specification {
     ObjectMapper objectMapper = Mock()
     NaverErrorDecoder errorDecoder = new NaverErrorDecoder(objectMapper)
 
-    def "에러디코더에서 에러발생시 RuntionException 예외가 throw된다."() {
+    def "에러디코더에서 에러발생시 ApiException 예외가 throw된다."() {
         given:
         def responseBody = Mock(Response.Body)
         def inputStream = new ByteArrayInputStream()
@@ -27,9 +30,11 @@ class NaverErrorDecoderTest extends Specification {
         errorDecoder.decode(_ as String, response)
 
         then:
-        RuntimeException e = thrown()
-        e.message == "error!!"
+        ApiException e = thrown()
+        verifyAll {
+            e.errorMessage == "error!!"
+            e.httpStatus == HttpStatus.BAD_REQUEST
+            e.errorType == ErrorType.EXTERNAL_API_ERROR
+        }
     }
-
-
 }
