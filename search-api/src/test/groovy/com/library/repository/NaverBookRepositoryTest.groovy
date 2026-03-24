54552acaf18e946006227418ebc676e0158671a1
gyucheol <gyucheol@talktomeinkorean.com>
2026-03-24 22:57:40 +0900

    [#3, LS-5] api 서버 구현 - 구조작성

---
+++ search-api/src/test/groovy/com/library/repository/NaverBookRepositoryTest.groovy
@@ -0,0 +1,51 @@
+package com.library.repository
+
+import com.library.Item
+import com.library.NaverBookResponse
+import com.library.feign.NaverClient
+import spock.lang.Specification
+
+import java.time.LocalDate
+
+class NaverBookRepositoryTest extends Specification {
+    BookRepository bookRepository
+
+    NaverClient naverClient = Mock()
+
+    void setup() {
+        bookRepository = new NaverBookRepository(naverClient)
+    }
+
+    def "search호출시 적절한 데이터 형식으로 변환한다."() {
+        given:
+
+        def items = [
+                new Item(title: "제목1", author: "저자1", publisher: "출판사1", pubDate: "20240322", isbn: "9788966261208"),
+                new Item(title: "제목2", author: "저자2", publisher: "출판사2", pubDate: "20240322", isbn: "9788966261208")
+        ]
+
+        def response = new NaverBookResponse(
+                lastBuildDate: "Sun, 22 Mar 2026 10:40:33 +0900",
+                total: 2,
+                start: 1,
+                display: 2,
+                items: items
+        )
+
+        and:
+        1 * naverClient.search("HTTP", 1, 2) >> response
+
+        when:
+        def result = bookRepository.search("HTTP", 1, 2)
+
+        then:
+        verifyAll {
+            result.size() == 2
+            result.page() == 1
+            result.totalElements() == 2
+            result.contents().size() == 2
+            result.contents().get(0).pubDate() == LocalDate.of(2024, 3, 22)
+        }
+    }
+
+}
